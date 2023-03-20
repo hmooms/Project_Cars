caf9d94517403e963231a93de73207f3fd8166eb
Alen Iriskic <cozyflexing@gmail.com>
2023-03-20 18:47:42 +0100

    new

--- src/main.cpp
+++ src/main.cpp
@@ -1,473 +1,164 @@
-#include <WebServer.h>
-#include <ESPmDNS.h>
 #include <Arduino.h>
-
-#include "../lib/Analog.h"
-#include "../lib/MotorDriver.h"
-
 #include <WiFi.h>
+#include "../lib/server.cpp"
 
+#define leftFrontIR 36
+#define rightFrontIR 39
+#define rightBackIR 5
+#define leftBackIR 27
+#define trigPin 21
+#define USEcho 34
+
+#define sensorStatusPin 12
 #define SOUND_SPEED 0.034
-#define CM_TO_INCH 0.393701
 
-const int trigPin = 21;
-const int echoFront = 34;
-const int echoBack = 35;
-const int echoRight = 14;
-const int echoLeft = 12;
-const int leftFrontIR = 36;
-const int rightFrontIR = 39;
-const int rightBackIR = 5;
-const int leftBackIR = 27;
-
-// Motor B linksvoor
-const int linksVoor1 = 32;   // Pin
-const int linksVoor2 = 33;   // Pin
-const int linksVoorPWM = 16; // Pin
-
-// Motor C linksachter
-const int linksAchter1 = 25;   // Pin
-const int linksAchter2 = 26;   // Pin
-const int linksAchterPWM = 15; // Pin
-
-// rechtsvoor
-const int rechtsVoor1 = 23;  // Pin
-const int rechtsVoor2 = 22;  // Pin
-const int rechtsVoorPWM = 4; // Pin
-
-// Motor D rechtsachter
-const int rechtsAchter1 = 18;   // Pin
-const int rechtsAchter2 = 19;   // Pin
-const int rechtsAchterPWM = 17; // Pin
+// Motor Pins
+int motorPins[][3] = {
+    {32, 33, 16}, // linksvoor
+    {25, 26, 15}, // linksachter
+    {23, 22, 4},  // rechtsvoor
+    {19, 13, 17}  // rechtsachter
+};
 
 long duration;
 float distanceCm;
 int state = 1;
 
-float distanceDetection(int pin)
+String sensorStatus = "off";
+
+float distanceDetection()
 {
-  // Clears the trigPin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  duration = pulseIn(pin, HIGH);
+  duration = pulseIn(USEcho, HIGH);
   float distanceCm = duration * SOUND_SPEED / 2;
   return distanceCm;
 }
 
 void setup()
 {
-  // Set pins as outputs
-  pinMode(rechtsVoor1, OUTPUT);
-  pinMode(rechtsVoor2, OUTPUT);
-  pinMode(rechtsVoorPWM, OUTPUT);
-  pinMode(linksVoor1, OUTPUT);
-  pinMode(linksVoor2, OUTPUT);
-  pinMode(linksVoorPWM, OUTPUT);
-  pinMode(linksAchter1, OUTPUT);
-  pinMode(linksAchter2, OUTPUT);
-  pinMode(linksAchterPWM, OUTPUT);
-  pinMode(rechtsAchter1, OUTPUT);
-  pinMode(rechtsAchter2, OUTPUT);
-  pinMode(rechtsAchterPWM, OUTPUT);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoFront, INPUT);
-  pinMode(echoBack, INPUT);
+  Serial.begin(9600);
+  pinMode(ACMStatusPin, OUTPUT);
+  pinMode(sensorStatusPin, OUTPUT);
+  for (int i = 0; i < 4; ++i)
+  {
+    for (int j = 0; j < 3; ++j)
+    {
+      pinMode(motorPins[i][j], OUTPUT);
+    }
+  }
   pinMode(leftFrontIR, INPUT);
   pinMode(rightFrontIR, INPUT);
-  Serial.begin(9600);
+  pinMode(rightBackIR, INPUT);
+  pinMode(leftBackIR, INPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(USEcho, INPUT);
+  setupServer();
 }
+
+void move(int mode, int time)
+{
+  static unsigned long startTime = millis();
+
+  if (time != 0 && millis() - startTime >= time)
+  {
+    return;
+  }
+
+  for (int i = 0; i < 4; ++i)
+  {
+    digitalWrite(motorPins[i][0], mode & (1 << i) ? HIGH : LOW);
+    digitalWrite(motorPins[i][1], mode & (1 << i) ? LOW : HIGH);
+    analogWrite(motorPins[i][2], 250);
+  }
+}
+
+void moveForward(int time) { move(0b1010, time); }
+void moveBackward(int time) { move(0b0101, time); }
+void moveLeft(int time) { move(0b1001, time); }
+void moveRight(int time) { move(0b0110, time); }
+void turnLeft(int time) { move(0b1100, time); }
+void turnRight(int time) { move(0b0011, time); }
+
 void stop()
 {
-  // Move forwards
-  digitalWrite(linksVoor1, LOW);
-  digitalWrite(linksVoor2, LOW);
-  digitalWrite(linksVoorPWM, LOW);
-  digitalWrite(linksAchter2, LOW);
-  digitalWrite(linksAchter1, LOW);
-  digitalWrite(linksAchterPWM, LOW);
-  digitalWrite(rechtsVoor1, LOW);
-  digitalWrite(rechtsVoor2, LOW);
-  digitalWrite(rechtsVoorPWM, LOW);
-  digitalWrite(rechtsAchter2, LOW);
-  digitalWrite(rechtsAchter1, LOW);
-  digitalWrite(rechtsAchterPWM, LOW); // set LED brightness as PWM signal
-}
-void moveForward(int x)
-{
-  if (x == 0)
+  for (int i = 0; i < 4; ++i)
   {
-    digitalWrite(linksVoor1, HIGH);
-    digitalWrite(linksVoor2, LOW);
-    digitalWrite(linksAchter2, HIGH);
-    digitalWrite(linksAchter1, LOW);
-    digitalWrite(rechtsVoor1, HIGH);
-    digitalWrite(rechtsVoor2, LOW);
-    digitalWrite(rechtsAchter2, HIGH);
-    digitalWrite(rechtsAchter1, LOW);
-    analogWrite(linksVoorPWM, 250);
-    analogWrite(linksAchterPWM, 250);
-    analogWrite(rechtsVoorPWM, 250);
-    analogWrite(rechtsAchterPWM, 250);
-  }
-  else
-  {
-    static unsigned long startTime = millis();
-    if (millis() - startTime < x)
+    for (int j = 0; j < 2; ++j)
     {
-      digitalWrite(linksVoor1, HIGH);
-      digitalWrite(linksVoor2, LOW);
-      digitalWrite(linksAchter2, HIGH);
-      digitalWrite(linksAchter1, LOW);
-      digitalWrite(rechtsVoor1, HIGH);
-      digitalWrite(rechtsVoor2, LOW);
-      digitalWrite(rechtsAchter2, HIGH);
-      digitalWrite(rechtsAchter1, LOW);
-      analogWrite(linksVoorPWM, 250);
-      analogWrite(linksAchterPWM, 250);
-      analogWrite(rechtsVoorPWM, 250);
-      analogWrite(rechtsAchterPWM, 250);
+      digitalWrite(motorPins[i][j], LOW);
     }
+    analogWrite(motorPins[i][2], LOW);
   }
-}
-void moveBackward(int x)
-{
-  if (x == 0)
-  {
-    digitalWrite(linksVoor1, LOW);
-    digitalWrite(linksVoor2, HIGH);
-    digitalWrite(linksAchter2, LOW);
-    digitalWrite(linksAchter1, HIGH);
-    digitalWrite(rechtsVoor1, LOW);
-    digitalWrite(rechtsVoor2, HIGH);
-    digitalWrite(rechtsAchter2, LOW);
-    digitalWrite(rechtsAchter1, HIGH);
-    analogWrite(linksVoorPWM, 100);
-    analogWrite(linksAchterPWM, 100);
-    analogWrite(rechtsVoorPWM, 100);
-    analogWrite(rechtsAchterPWM, 100);
-  }
-  else
-  {
-    static unsigned long startTime = millis();
-    if (millis() - startTime < x)
-    {
-      digitalWrite(linksVoor1, LOW);
-      digitalWrite(linksVoor2, HIGH);
-      digitalWrite(linksAchter2, LOW);
-      digitalWrite(linksAchter1, HIGH);
-      digitalWrite(rechtsVoor1, LOW);
-      digitalWrite(rechtsVoor2, HIGH);
-      digitalWrite(rechtsAchter2, LOW);
-      digitalWrite(rechtsAchter1, HIGH);
-      analogWrite(linksVoorPWM, 100);
-      analogWrite(linksAchterPWM, 100);
-      analogWrite(rechtsVoorPWM, 100);
-      analogWrite(rechtsAchterPWM, 100);
-    }
-  }
-}
-void moveDiagonalRight(int x)
-{
-  if (x == 0)
-  {
-    digitalWrite(linksVoor1, HIGH);
-    digitalWrite(linksVoor2, LOW);
-    digitalWrite(linksAchter2, LOW);
-    digitalWrite(linksAchter1, LOW);
-    digitalWrite(rechtsVoor1, LOW);
-    digitalWrite(rechtsVoor2, LOW);
-    digitalWrite(rechtsAchter2, HIGH);
-    digitalWrite(rechtsAchter1, LOW);
-    analogWrite(linksVoorPWM, 250);
-    analogWrite(linksAchterPWM, 0);
-    analogWrite(rechtsVoorPWM, 0);
-    analogWrite(rechtsAchterPWM, 250);
-  }
-  else
-  {
-    static unsigned long startTime = millis();
-    if (millis() - startTime < x)
-    {
-      digitalWrite(linksVoor1, HIGH);
-      digitalWrite(linksVoor2, LOW);
-      digitalWrite(linksAchter2, LOW);
-      digitalWrite(linksAchter1, LOW);
-      digitalWrite(rechtsVoor1, LOW);
-      digitalWrite(rechtsVoor2, LOW);
-      digitalWrite(rechtsAchter2, HIGH);
-      digitalWrite(rechtsAchter1, LOW);
-      analogWrite(linksVoorPWM, 250);
-      analogWrite(linksAchterPWM, 0);
-      analogWrite(rechtsVoorPWM, 0);
-      analogWrite(rechtsAchterPWM, 250);
-    }
-  }
-}
-void moveDiagonalLeft(int x)
-{
-  if (x == 0)
-  {
-    digitalWrite(linksVoor1, LOW);
-    digitalWrite(linksVoor2, LOW);
-    digitalWrite(linksAchter2, HIGH);
-    digitalWrite(linksAchter1, LOW);
-    digitalWrite(rechtsVoor1, HIGH);
-    digitalWrite(rechtsVoor2, LOW);
-    digitalWrite(rechtsAchter2, LOW);
-    digitalWrite(rechtsAchter1, LOW);
-    analogWrite(linksVoorPWM, 0);
-    analogWrite(linksAchterPWM, 250);
-    analogWrite(rechtsVoorPWM, 250);
-    analogWrite(rechtsAchterPWM, 0);
-  }
-  else
-  {
-    static unsigned long startTime = millis();
-    if (millis() - startTime < x)
-    {
-      digitalWrite(linksVoor1, LOW);
-      digitalWrite(linksVoor2, LOW);
-      digitalWrite(linksAchter2, HIGH);
-      digitalWrite(linksAchter1, LOW);
-      digitalWrite(rechtsVoor1, HIGH);
-      digitalWrite(rechtsVoor2, LOW);
-      digitalWrite(rechtsAchter2, LOW);
-      digitalWrite(rechtsAchter1, LOW);
-      analogWrite(linksVoorPWM, 0);
-      analogWrite(linksAchterPWM, 250);
-      analogWrite(rechtsVoorPWM, 250);
-      analogWrite(rechtsAchterPWM, 0);
-    }
-  }
-}
-void moveLeft(int x)
-{
-  if (x == 0)
-  {
-    digitalWrite(rechtsVoor1, HIGH);
-    digitalWrite(rechtsVoor2, LOW);
-    digitalWrite(linksVoor1, LOW);
-    digitalWrite(linksVoor2, HIGH);
-    digitalWrite(linksAchter2, HIGH);
-    digitalWrite(linksAchter1, LOW);
-    digitalWrite(rechtsAchter2, LOW);
-    digitalWrite(rechtsAchter1, HIGH);
-    analogWrite(linksVoorPWM, 250);
-    analogWrite(linksAchterPWM, 250);
-    analogWrite(rechtsVoorPWM, 250);
-    analogWrite(rechtsAchterPWM, 250);
-  }
-  else
-  {
-    static unsigned long startTime = millis();
-    if (millis() - startTime < x)
-    {
-      digitalWrite(rechtsVoor1, HIGH);
-      digitalWrite(rechtsVoor2, LOW);
-      digitalWrite(linksVoor1, LOW);
-      digitalWrite(linksVoor2, HIGH);
-      digitalWrite(linksAchter2, HIGH);
-      digitalWrite(linksAchter1, LOW);
-      digitalWrite(rechtsAchter2, LOW);
-      digitalWrite(rechtsAchter1, HIGH);
-      analogWrite(linksVoorPWM, 250);
-      analogWrite(linksAchterPWM, 250);
-      analogWrite(rechtsVoorPWM, 250);
-      analogWrite(rechtsAchterPWM, 250);
-    }
-  }
-}
-void moveRight(int x)
-{
-  if (x == 0)
-  {
-    digitalWrite(rechtsVoor1, LOW);
-    digitalWrite(rechtsVoor2, HIGH);
-    digitalWrite(linksVoor1, HIGH);
-    digitalWrite(linksVoor2, LOW);
-    digitalWrite(linksAchter2, LOW);
-    digitalWrite(linksAchter1, HIGH);
-    digitalWrite(rechtsAchter2, HIGH);
-    digitalWrite(rechtsAchter1, LOW);
-    analogWrite(linksVoorPWM, 250);
-    analogWrite(linksAchterPWM, 250);
-    analogWrite(rechtsVoorPWM, 250);
-    analogWrite(rechtsAchterPWM, 250);
-  }
-  else
-  {
-    static unsigned long startTime = millis();
-    if (millis() - startTime < x)
-    {
-      digitalWrite(rechtsVoor1, LOW);
-      digitalWrite(rechtsVoor2, HIGH);
-      digitalWrite(linksVoor1, HIGH);
-      digitalWrite(linksVoor2, LOW);
-      digitalWrite(linksAchter2, LOW);
-      digitalWrite(linksAchter1, HIGH);
-      digitalWrite(rechtsAchter2, HIGH);
-      digitalWrite(rechtsAchter1, LOW);
-      analogWrite(linksVoorPWM, 250);
-      analogWrite(linksAchterPWM, 250);
-      analogWrite(rechtsVoorPWM, 250);
-      analogWrite(rechtsAchterPWM, 250);
-    }
-  }
-}
-void turnLeft(int x)
-{
-  if (x == 0)
-  {
-    digitalWrite(rechtsVoor1, HIGH);
-    digitalWrite(rechtsVoor2, LOW);
-    digitalWrite(linksVoor1, LOW);
-    digitalWrite(linksVoor2, HIGH);
-    digitalWrite(linksAchter2, LOW);
-    digitalWrite(linksAchter1, HIGH);
-    digitalWrite(rechtsAchter2, HIGH);
-    digitalWrite(rechtsAchter1, LOW);
-    analogWrite(linksVoorPWM, 250);
-    analogWrite(linksAchterPWM, 250);
-    analogWrite(rechtsVoorPWM, 250);
-    analogWrite(rechtsAchterPWM, 250);
-  }
-  else
-  {
-    static unsigned long startTime = millis();
-    if (millis() - startTime < x)
-    {
-      digitalWrite(rechtsVoor1, HIGH);
-      digitalWrite(rechtsVoor2, LOW);
-      digitalWrite(linksVoor1, LOW);
-      digitalWrite(linksVoor2, HIGH);
-      digitalWrite(linksAchter2, LOW);
-      digitalWrite(linksAchter1, HIGH);
-      digitalWrite(rechtsAchter2, HIGH);
-      digitalWrite(rechtsAchter1, LOW);
-      analogWrite(linksVoorPWM, 250);
-      analogWrite(linksAchterPWM, 250);
-      analogWrite(rechtsVoorPWM, 250);
-      analogWrite(rechtsAchterPWM, 250);
-    }
-  }
-}
-void turnRight(int x)
-{
-  if (x == 0)
-  {
-    digitalWrite(rechtsVoor1, LOW);
-    digitalWrite(rechtsVoor2, HIGH);
-    digitalWrite(linksVoor1, HIGH);
-    digitalWrite(linksVoor2, LOW);
-    digitalWrite(linksAchter2, HIGH);
-    digitalWrite(linksAchter1, LOW);
-    digitalWrite(rechtsAchter2, LOW);
-    digitalWrite(rechtsAchter1, HIGH);
-    analogWrite(linksVoorPWM, 250);
-    analogWrite(linksAchterPWM, 250);
-    analogWrite(rechtsVoorPWM, 250);
-    analogWrite(rechtsAchterPWM, 250);
-  }
-  else
-  {
-    static unsigned long startTime = millis();
-    if (millis() - startTime < x)
-    {
-      digitalWrite(rechtsVoor1, LOW);
-      digitalWrite(rechtsVoor2, HIGH);
-      digitalWrite(linksVoor1, HIGH);
-      digitalWrite(linksVoor2, LOW);
-      digitalWrite(linksAchter2, HIGH);
-      digitalWrite(linksAchter1, LOW);
-      digitalWrite(rechtsAchter2, LOW);
-      digitalWrite(rechtsAchter1, HIGH);
-      analogWrite(linksVoorPWM, 250);
-      analogWrite(linksAchterPWM, 250);
-      analogWrite(rechtsVoorPWM, 250);
-      analogWrite(rechtsAchterPWM, 250);
-    }
-  }
-}
-
-void hugRightLine()
-{
-  while (digitalRead(rightFrontIR) && digitalRead(rightBackIR))
-  {
-    moveRight(0);
-  }
-  while (!digitalRead(rightFrontIR) && digitalRead(rightBackIR))
-  {
-    turnLeft(0);
-  }
-  while (digitalRead(rightFrontIR) && !digitalRead(rightBackIR))
-  {
-    turnRight(0);
-  }
-  stop();
-}
-
-void hugLeftLine()
-{
-  while (digitalRead(leftBackIR) && digitalRead(leftFrontIR))
-  {
-    moveLeft(0);
-  }
-  while (!digitalRead(leftBackIR) && digitalRead(leftFrontIR))
-  {
-    turnLeft(0);
-  }
-  while (digitalRead(leftBackIR) && !digitalRead(leftFrontIR))
-  {
-    turnRight(0);
-  }
-}
-
-void spaceWalk()
-{
 }
 
 void loop()
 {
-  switch (state)
-  {
+  // checkForClients();
+  // float distance = distanceDetection();
 
-  case 0: // Backward
-    moveBackward(0);
-    break;
+  // if (ACMStatusPinState == "on")
+  // {
+  //   digitalWrite(ACMStatusPin, HIGH);
 
-  case 1: // Forward
-    moveForward(0);
-    break;
+  //   if (digitalRead(leftFrontIR) || digitalRead(rightFrontIR) || digitalRead(leftBackIR) || digitalRead(rightBackIR))
+  //   {
+  //     sensorStatus = "IR";
+  //   }
+  //   else if (distance < 5)
+  //   {
+  //     sensorStatus = "alert";
+  //   }
+  //   else if (distance < 20)
+  //   {
+  //     sensorStatus = "US";
+  //   }
+  //   else
+  //   {
+  //     sensorStatus = "off";
+  //   }
+  // moveForward(0);
+  analogWrite(19, HIGH); // Set pin 19 to HIGH
+  analogWrite(18, HIGH); // Set pin 18 to LOW
+  analogWrite(17, 250);  // Set PWM to pin 17
 
-  case 2: // Diagonal right
-    moveDiagonalRight(0);
-    break;
+  delay(2000); // Wait for 2 seconds
 
-  case 3: // Diagonal left
-    moveDiagonalLeft(0);
-    break;
+  analogWrite(19, HIGH); // Set pin 19 to LOW
+  analogWrite(18, HIGH); // Set pin 18 to HIGH
+  analogWrite(17, 250);  // Set PWM to pin 17
 
-  case 4: // Left
-    moveLeft(0);
-    break;
+  delay(2000); // Wait for 2 seconds
+  // }
+  // else
+  // {
+  //   stop();
+  //   digitalWrite(ACMStatusPin, LOW);
+  // }
 
-  case 5: // Right
-    moveRight(0);
-    break;
-
-  case 6: // Turn left
-    turnLeft(0);
-    break;
-
-  case 7: // Turn right
-    turnRight(0);
-    break;
-
-  default: // Stop
-    stop();
-  }
+  // if (sensorStatus == "off")
+  // {
+  //   digitalWrite(sensorStatusPin, LOW);
+  // }
+  // else if (sensorStatus == "IR")
+  // {
+  //   digitalWrite(sensorStatusPin, HIGH);
+  // }
+  // else if (sensorStatus == "US")
+  // {
+  //   digitalWrite(sensorStatusPin, HIGH);
+  //   delay(500);
+  //   digitalWrite(sensorStatusPin, LOW);
+  //   delay(500);
+  // }
+  // else if (sensorStatus == "alert")
+  // {
+  //   digitalWrite(sensorStatusPin, HIGH);
+  //   delay(250);
+  //   digitalWrite(sensorStatusPin, LOW);
+  //   delay(250);
+  // }
 }
